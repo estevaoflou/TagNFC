5e22741a3e5bf2d8920268740428cdab0279ecac
estevaoflou <56273355+estevaoflou@users.noreply.github.com>
2019-10-15 15:32:15 -0300

    Initial commit

---
+++ app/src/main/java/com/example/tagnfc/parser/NdefMessageParser.java
@@ -0,0 +1,45 @@
+package com.example.tagnfc.parser;
+
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+
+import com.example.tagnfc.record.ParsedNdefRecord;
+import com.example.tagnfc.record.SmartPoster;
+import com.example.tagnfc.record.TextRecord;
+import com.example.tagnfc.record.UriRecord;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NdefMessageParser {
+
+    private NdefMessageParser() {
+    }
+
+    public static List<ParsedNdefRecord> parse(NdefMessage message) {
+        return getRecords(message.getRecords());
+    }
+
+    public static List<ParsedNdefRecord> getRecords(NdefRecord[] records) {
+        List<ParsedNdefRecord> elements = new ArrayList<ParsedNdefRecord>();
+
+        for (final NdefRecord record : records) {
+            if (UriRecord.isUri(record)) {
+                elements.add(UriRecord.parse(record));
+            } else if (TextRecord.isText(record)) {
+                elements.add(TextRecord.parse(record));
+            } else if (SmartPoster.isPoster(record)) {
+                elements.add(SmartPoster.parse(record));
+            } else {
+                elements.add(new ParsedNdefRecord() {
+                    public String str() {
+                        return new String(record.getPayload());
+                    }
+                });
+            }
+        }
+
+        return elements;
+    }
+
+}
---
+++ app/src/main/java/com/example/tagnfc/record/ParsedNdefRecord.java
@@ -0,0 +1,7 @@
+package com.example.tagnfc.record;
+
+public interface ParsedNdefRecord {
+
+    String str();
+
+}
